454d173bc7656e8bf7ff866bae3a8bb7f8f6df80
Bilel Marouen <marouen.bilel@gmail.com>
2024-12-27 14:15:03 +0000

    Mise à jour de MyDatabaseContext pour utiliser Microsoft.Data.SqlClient et ajout de vérifications null pour HttpContext et les en-têtes

--- Data/MyDatabaseContext.cs
+++ Data/MyDatabaseContext.cs
@@ -13,8 +13,11 @@ namespace DotNetCoreSqlDb.Data
         public MyDatabaseContext (DbContextOptions<MyDatabaseContext> options, IHttpContextAccessor accessor)
             : base(options)
         {
-            var conn = Database.GetDbConnection() as SqlConnection;
-            conn.AccessToken = accessor.HttpContext.Request.Headers["X-MS-TOKEN-AAD-ACCESS-TOKEN"];
+            var conn = Database.GetDbConnection() as Microsoft.Data.SqlClient.SqlConnection;
+            if (conn != null && accessor.HttpContext?.Request?.Headers != null)
+            {
+                conn.AccessToken = accessor.HttpContext.Request.Headers["X-MS-TOKEN-AAD-ACCESS-TOKEN"];
+            }
         }
 
         public DbSet<DotNetCoreSqlDb.Models.Todo> Todo { get; set; } = default!;
