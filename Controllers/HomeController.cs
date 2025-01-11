fb947571341097fb64664e36aca2fe23da884863
bilel <marouen.bilel@gmail.com>
2025-01-11 07:47:33 +0100

    Add DisplayHtml action to HomeController and corresponding view for rendering HTML from Azure Blob Storage

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using DotNetCoreSqlDb.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace DotNetCoreSqlDb.Controllers
 {
@@ -26,6 +28,21 @@ namespace DotNetCoreSqlDb.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> DisplayHtml()
+        {
+            string htmlContent;
+            using (var httpClient = new HttpClient())
+            {
+                // Replace with your storage container URL
+                var response = await httpClient.GetAsync("https://serviceconnector01.blob.core.windows.net/demo/index.html");
+                response.EnsureSuccessStatusCode();
+                htmlContent = await response.Content.ReadAsStringAsync();
+            }
+
+            ViewData["HtmlContent"] = htmlContent;
+            return View();
         }
     }
 }
